5f96a7b1c761648a5aaaa32aaafc820d4a5cc483
ssedeff <145463803+ssedeff@users.noreply.github.com>
2024-01-02 14:12:45 +0300

    Add files via upload

---
+++ doğumgünü-günsayisi/doğumgünü-günsayisi/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace doğumgünü_günsayisi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DateTime dogumTarihi=new DateTime(2024,9,27);
+            DateTime bugun = DateTime.Today;
+            TimeSpan kalansure=dogumTarihi-bugun;
+            int kalangun = kalansure.Days;
+            label1.Text = kalangun.ToString();
+        }
+    }
+}
